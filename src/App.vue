26ddafb0d122873fe6b7046eb275dda32a6f7c36
Lararail <misuzu8080@gmail.com>
2021-04-28 09:00:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Signup />
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Signup from './components/Signup.vue'
+
+export default {
+  name: 'App',
+  components: { Signup }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin:0;
+  background:#eee;
+}
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,160 @@
+<template>
+  <form @submit.prevent="handleSubmit"> <!---prevent submitボタンを押しても記録を残す-->
+      <label>Email:</label>
+      <input type="email" required v-model="email">
+
+      <label>Password:</label>
+      <input type="password" required v-model="password">
+      <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+
+    <label>Skills: </label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill"><!--@keyup.alt="addskill"-->
+    <div v-for="skill in skills" :key="skill" class="pill"><!--:key="skill"  ユニークなもの-->
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="term">
+        <input type="checkbox" required v-model="terms">
+        <label>Accept terms and conditions</label>
+    </div>
+    <!-- <div>
+        <input type="checkbox" value="yoshi" v-model="names">
+        <label>yoshi</label>
+    </div>
+    <div>
+        <input type="checkbox" value="mario" v-model="names">
+        <label>mario</label>
+    </div>
+    <div>    
+        <input type="checkbox" value="misuzu" v-model="names">
+        <label>misuzu</label>
+    </div> -->
+
+    <div class="submit">
+        <button>Create an account</button>
+    </div>
+  </form>
+    <p>Email: {{ email }}</p>
+    <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+    <p>Terms accepted: {{ terms }}</p>
+    <!-- <p>names: {{ names }}</p> -->
+    <!-- <p>Skills: {{ skills }}</p> -->
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+            role: '',
+            terms: false,
+            //names: []
+            tempSkill:'',
+            skills: [],
+            passwordError:''
+        }
+    },
+    methods:{
+        addSkill(e){
+            //console.log(e)
+            if(e.key === ',' && this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+            this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill){
+            this.skills = this.skills.filter((item)=>{  //filter if false then remove item from array
+                return skill !== item
+            })
+        },
+        handleSubmit(){
+            console.log('submit')
+            //validate password
+            this.passwordError = this.password.length > 5 ? 
+            '':'password must be 6 chars long'
+            if(!this.passwordError){
+                console.log('email: ',this.email)
+                console.log('password: ',this.password)
+                console.log('role: ',this.role)
+                console.log('term accepted: ',this.terms)
+            }
+
+        }
+    }
+}
+</script>
+
+<style>
+form{
+    max-width:420px;
+    margin:30px auto;
+    background:white;
+    text-align:left;
+    padding:40px;
+    border-radius:10px;
+}
+label{
+    color:#aaa;
+    display:inline-block;
+    margin:25px 0 25px;
+    font-size:0.6em;
+    text-transform:uppercase;
+    letter-spacing:1px;
+    font-weight:bold;
+}
+input, select{
+    display:block;
+    padding:10px 6px;
+    width:100%;
+    box-sizing:border-box;
+    border:none;
+    border-bottom:1px solid #ddd;
+    color:#555;
+}
+input[type="checkbox"]{
+    display:inline-block;
+    width:16px;
+    margin:0 10px 0 0;
+    position:relative;
+    top:2px;
+}
+.pill{
+    display:inline-block;
+    margin:20px 10px 0 0;
+    padding:6px 12px;
+    background:#eee;
+    border-radius:20px;
+    font-size:12px;
+    letter-spacing:1px;
+    font-weight:bold;
+    color:#777;
+    cursor:pointer;
+}
+button{
+    background:#0b6dff;
+    border:0px;
+    padding:10px 20px;
+    margin-top:20px;
+    color:white;
+    border-radius:20px;
+}
+.submit{
+    text-align:center;
+}
+.error{
+    color:#ff0062;
+    margin-top:20px;
+    font-size:0.8em;
+    font-weight:bold;
+}
+</style>
